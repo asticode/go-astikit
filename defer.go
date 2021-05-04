15ad02f48d8b6e6bb3552190b5078c822b2ac12a
Quentin Renard <contact@asticode.com>
2021-05-04 11:19:08 +0200

    Added Closer.Do()

--- defer.go
+++ defer.go
@@ -9,31 +9,43 @@ type CloseFunc func() error
 
 // Closer is an object that can close several things
 type Closer struct {
-	fs []CloseFunc
-	m  *sync.Mutex
+	closed bool
+	fs     []CloseFunc
+	// We need to split into 2 mutexes to allow using .Add() in .Do()
+	mc *sync.Mutex // Locks .Close()
+	mf *sync.Mutex // Locks fs
 }
 
 // NewCloser creates a new closer
 func NewCloser() *Closer {
 	return &Closer{
-		m: &sync.Mutex{},
+		mc: &sync.Mutex{},
+		mf: &sync.Mutex{},
 	}
 }
 
 // Close implements the io.Closer interface
 func (c *Closer) Close() error {
 	// Lock
-	c.m.Lock()
-	defer c.m.Unlock()
+	c.mc.Lock()
+	defer c.mc.Unlock()
+
+	// Get funcs
+	c.mf.Lock()
+	fs := c.fs
+	c.mf.Unlock()
 
 	// Loop through closers
 	err := NewErrors()
-	for _, f := range c.fs {
+	for _, f := range fs {
 		err.Add(f())
 	}
 
 	// Reset closers
 	c.fs = []CloseFunc{}
+
+	// Update attribute
+	c.closed = true
 
 	// Return
 	if err.IsNil() {
@@ -44,8 +56,11 @@ func (c *Closer) Close() error {
 
 // Add adds a close func at the beginning of the list
 func (c *Closer) Add(f CloseFunc) {
-	c.m.Lock()
-	defer c.m.Unlock()
+	// Lock
+	c.mf.Lock()
+	defer c.mf.Unlock()
+
+	// Append
 	c.fs = append([]CloseFunc{f}, c.fs...)
 }
 
@@ -54,4 +69,21 @@ func (c *Closer) NewChild() (child *Closer) {
 	child = NewCloser()
 	c.Add(child.Close)
 	return
+}
+
+// Do executes a callback while ensuring :
+//   - closer hasn't been closed before
+//   - closer can't be closed in between
+func (c *Closer) Do(fn func()) {
+	// Lock
+	c.mc.Lock()
+	defer c.mc.Unlock()
+
+	// Closer already closed
+	if c.closed {
+		return
+	}
+
+	// Callback
+	fn()
 }
