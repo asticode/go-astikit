31f2c16fdf0b6ea0aa4d8e9a2d20a3afafe7e8e2
Quentin Renard <contact@asticode.com>
2021-10-06 09:41:29 +0200

    Added json clone

--- json.go
+++ json.go
@@ -3,6 +3,7 @@ package astikit
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 )
 
 func JSONEqual(a, b interface{}) bool {
@@ -15,4 +16,20 @@ func JSONEqual(a, b interface{}) bool {
 		return false
 	}
 	return bytes.Equal(ba, bb)
+}
+
+func JSONClone(src, dst interface{}) (err error) {
+	// Marshal
+	var b []byte
+	if b, err = json.Marshal(src); err != nil {
+		err = fmt.Errorf("main: marshaling failed: %w", err)
+		return
+	}
+
+	// Unmarshal
+	if err = json.Unmarshal(b, dst); err != nil {
+		err = fmt.Errorf("main: unmarshaling failed: %w", err)
+		return
+	}
+	return
 }
--- json_test.go
+++ json_test.go
@@ -10,6 +10,18 @@ type jsonB struct {
 	B string `json:"a"`
 }
 
+func TestJSONClone(t *testing.T) {
+	a := jsonA{A: "a"}
+	b := &jsonB{}
+	err := JSONClone(a, b)
+	if err != nil {
+		t.Errorf("expected no error, got %s", err)
+	}
+	if !JSONEqual(a, b) {
+		t.Error("expected true, got false")
+	}
+}
+
 func TestJSONEqual(t *testing.T) {
 	if JSONEqual(jsonA{A: "a"}, jsonB{B: "b"}) {
 		t.Error("expected false, got true")
