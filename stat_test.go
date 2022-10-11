07596dbc01e6bb13d08c9d57cf373ac655b60fa3
Quentin Renard <contact@asticode.com>
2022-10-11 11:09:52 +0200

    Updated stats

--- stat_test.go
+++ stat_test.go
@@ -23,15 +23,19 @@ func TestStater(t *testing.T) {
 	}
 
 	// Add stats
-	h1 := NewCounterRateStat()
+	v1 := NewCounterRateStat()
 	m1 := &StatMetadata{Description: "1"}
-	o1 := StatOptions{Handler: h1, Metadata: m1}
-	h2 := NewDurationPercentageStat()
+	o1 := StatOptions{Metadata: m1, Valuer: v1}
+	v2 := NewDurationPercentageStat()
 	m2 := &StatMetadata{Description: "2"}
-	o2 := StatOptions{Handler: h2, Metadata: m2}
-	h3 := NewCounterAvgStat()
+	o2 := StatOptions{Metadata: m2, Valuer: v2}
+	v3 := NewCounterAvgStat()
 	m3 := &StatMetadata{Description: "3"}
-	o3 := StatOptions{Handler: h3, Metadata: m3}
+	o3 := StatOptions{Metadata: m3, Valuer: v3}
+	v4 := NewCounterStat()
+	v4.Add(1)
+	m4 := &StatMetadata{Description: "4"}
+	o4 := StatOptions{Metadata: m4, Valuer: v4}
 
 	// First time stats are computed, it actually acts as if stats were being updated
 	// Second time stats are computed, results are stored and context is cancelled
@@ -44,18 +48,19 @@ func TestStater(t *testing.T) {
 			c++
 			switch c {
 			case 1:
-				h1.Add(10)
+				v1.Add(10)
 				mn.Lock()
 				nowV = time.Unix(0, 0)
 				mn.Unlock()
-				h2.Begin()
+				v2.Begin()
 				mn.Lock()
 				nowV = time.Unix(5, 0)
 				mn.Unlock()
-				h2.End()
-				h3.Add(10)
-				h3.Add(20)
-				h3.Add(30)
+				v2.End()
+				v3.Add(10)
+				v3.Add(20)
+				v3.Add(30)
+				v4.Add(1)
 			case 2:
 				ss = stats
 				cancel()
@@ -63,17 +68,14 @@ func TestStater(t *testing.T) {
 		},
 		Period: time.Millisecond,
 	})
-	s.AddStats(o1, o2, o3)
-	for _, o := range []StatOptions{o1, o2, o3} {
-		o.Handler.Start()
-		defer o.Handler.Stop()
-	}
+	s.AddStats(o1, o2, o3, o4)
 	s.Start(ctx)
 	defer s.Stop()
 	for _, e := range []StatValue{
 		{StatMetadata: m1, Value: 2.0},
 		{StatMetadata: m2, Value: 100.0},
 		{StatMetadata: m3, Value: 20.0},
+		{StatMetadata: m4, Value: 2.0},
 	} {
 		found := false
 		for _, s := range ss {
