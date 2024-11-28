20c3293761c52d103c8857d97274e6d328e73a86
Quentin Renard <contact-github@asticode.com>
2024-11-28 09:40:07 +0100

    Added TimestampNano and Stopwatch

--- time.go
+++ time.go
@@ -2,8 +2,12 @@ package astikit
 
 import (
 	"context"
+	"encoding"
+	"encoding/json"
+	"fmt"
 	"io"
 	"strconv"
+	"strings"
 	"time"
 )
 
@@ -18,42 +22,6 @@ func Sleep(ctx context.Context, d time.Duration) (err error) {
 			return
 		}
 	}
-}
-
-// Timestamp represents a timestamp you can marshal and umarshal
-type Timestamp struct {
-	time.Time
-}
-
-// NewTimestamp creates a new timestamp
-func NewTimestamp(t time.Time) *Timestamp {
-	return &Timestamp{Time: t}
-}
-
-// UnmarshalJSON implements the JSONUnmarshaler interface
-func (t *Timestamp) UnmarshalJSON(text []byte) error {
-	return t.UnmarshalText(text)
-}
-
-// UnmarshalText implements the TextUnmarshaler interface
-func (t *Timestamp) UnmarshalText(text []byte) (err error) {
-	var i int
-	if i, err = strconv.Atoi(string(text)); err != nil {
-		return
-	}
-	t.Time = time.Unix(int64(i), 0)
-	return
-}
-
-// MarshalJSON implements the JSONMarshaler interface
-func (t Timestamp) MarshalJSON() ([]byte, error) {
-	return t.MarshalText()
-}
-
-// MarshalText implements the TextMarshaler interface
-func (t Timestamp) MarshalText() (text []byte, err error) {
-	text = []byte(strconv.Itoa(int(t.UTC().Unix())))
-	return
 }
 
 var now = time.Now
@@ -79,4 +47,236 @@ func MockNow(fn func() time.Time) io.Closer {
 	m := newMockedNow()
 	now = fn
 	return m
+}
+
+var (
+	_ encoding.TextMarshaler   = (*Timestamp)(nil)
+	_ encoding.TextUnmarshaler = (*Timestamp)(nil)
+	_ json.Marshaler           = (*Timestamp)(nil)
+	_ json.Unmarshaler         = (*Timestamp)(nil)
+)
+
+type Timestamp struct {
+	time.Time
+}
+
+func NewTimestamp(t time.Time) *Timestamp {
+	return &Timestamp{Time: t}
+}
+
+func (t *Timestamp) UnmarshalJSON(text []byte) error {
+	return t.UnmarshalText(text)
+}
+
+func (t *Timestamp) UnmarshalText(text []byte) (err error) {
+	var i int
+	if i, err = strconv.Atoi(string(text)); err != nil {
+		return
+	}
+	t.Time = time.Unix(int64(i), 0)
+	return
+}
+
+func (t Timestamp) MarshalJSON() ([]byte, error) {
+	return t.MarshalText()
+}
+
+func (t Timestamp) MarshalText() (text []byte, err error) {
+	text = []byte(strconv.Itoa(int(t.UTC().Unix())))
+	return
+}
+
+var (
+	_ encoding.TextMarshaler   = (*TimestampNano)(nil)
+	_ encoding.TextUnmarshaler = (*TimestampNano)(nil)
+	_ json.Marshaler           = (*TimestampNano)(nil)
+	_ json.Unmarshaler         = (*TimestampNano)(nil)
+)
+
+type TimestampNano struct {
+	time.Time
+}
+
+func NewTimestampNano(t time.Time) *TimestampNano {
+	return &TimestampNano{Time: t}
+}
+
+func (t *TimestampNano) UnmarshalJSON(text []byte) error {
+	return t.UnmarshalText(text)
+}
+
+func (t *TimestampNano) UnmarshalText(text []byte) (err error) {
+	var i int
+	if i, err = strconv.Atoi(string(text)); err != nil {
+		return
+	}
+	t.Time = time.Unix(0, int64(i))
+	return
+}
+
+func (t TimestampNano) MarshalJSON() ([]byte, error) {
+	return t.MarshalText()
+}
+
+func (t TimestampNano) MarshalText() (text []byte, err error) {
+	text = []byte(strconv.Itoa(int(t.UTC().UnixNano())))
+	return
+}
+
+var (
+	_ json.Marshaler   = (*Stopwatch)(nil)
+	_ json.Unmarshaler = (*Stopwatch)(nil)
+)
+
+type Stopwatch struct {
+	children  []*Stopwatch
+	createdAt time.Time
+	doneAt    time.Time
+	label     string
+}
+
+func NewStopwatch() *Stopwatch {
+	return newStopwatch("")
+}
+
+func newStopwatch(label string) *Stopwatch {
+	return &Stopwatch{
+		createdAt: Now(),
+		label:     label,
+	}
+}
+
+func (s *Stopwatch) NewChild(label string) *Stopwatch {
+	// Create stopwatch
+	dst := newStopwatch(label)
+
+	// Make sure to propagate done to children
+	s.propagateDone(dst.createdAt)
+
+	// Append
+	s.children = append(s.children, dst)
+	return dst
+}
+
+func (s *Stopwatch) propagateDone(doneAt time.Time) {
+	// No children
+	if len(s.children) == 0 {
+		return
+	}
+
+	// Get child
+	c := s.children[len(s.children)-1]
+
+	// Update done at
+	if c.doneAt.IsZero() {
+		c.doneAt = doneAt
+	}
+
+	// Make sure to propagate done to children
+	c.propagateDone(doneAt)
+}
+
+func (s *Stopwatch) Done() {
+	// Update done at
+	if s.doneAt.IsZero() {
+		s.doneAt = Now()
+	}
+
+	// Make sure to propagate done to children
+	s.propagateDone(s.doneAt)
+}
+
+func (s *Stopwatch) Duration() time.Duration {
+	if !s.doneAt.IsZero() {
+		return s.doneAt.Sub(s.createdAt)
+	}
+	return Now().Sub(s.createdAt)
+}
+
+func (s *Stopwatch) Merge(i *Stopwatch) {
+	// No children
+	if len(i.children) == 0 {
+		return
+	}
+
+	// Make sure to propagate done to children
+	s.propagateDone(i.children[0].createdAt)
+
+	// Append
+	s.children = append(s.children, i.children...)
+}
+
+func (s *Stopwatch) Dump() string {
+	return s.dump("", s.createdAt)
+}
+
+func (s *Stopwatch) dump(ident string, rootCreatedAt time.Time) string {
+	// Dump stopwatch
+	var ss []string
+	if ident == "" {
+		ss = append(ss, DurationMinimalistFormat(s.doneAt.Sub(s.createdAt)))
+	} else {
+		ss = append(ss, fmt.Sprintf("%s[%s]%s: %s", ident, DurationMinimalistFormat(s.createdAt.Sub(rootCreatedAt)), s.label, DurationMinimalistFormat(s.doneAt.Sub(s.createdAt))))
+	}
+
+	// Loop through children
+	ident += "  "
+	for _, c := range s.children {
+		// Dump child
+		ss = append(ss, c.dump(ident, rootCreatedAt))
+	}
+	return strings.Join(ss, "\n")
+}
+
+type stopwatchJSON struct {
+	Children  []stopwatchJSON `json:"children"`
+	CreatedAt TimestampNano   `json:"created_at"`
+	DoneAt    TimestampNano   `json:"done_at"`
+	Label     string          `json:"label"`
+}
+
+func (sj stopwatchJSON) toStopwatch(s *Stopwatch) {
+	s.createdAt = sj.CreatedAt.Time
+	s.doneAt = sj.DoneAt.Time
+	s.label = sj.Label
+	for _, cj := range sj.Children {
+		c := &Stopwatch{}
+		cj.toStopwatch(c)
+		s.children = append(s.children, c)
+	}
+}
+
+func (s *Stopwatch) toStopwatchJSON() (sj stopwatchJSON) {
+	sj.Children = []stopwatchJSON{}
+	sj.CreatedAt = *NewTimestampNano(s.createdAt)
+	sj.DoneAt = *NewTimestampNano(s.doneAt)
+	sj.Label = s.label
+	for _, c := range s.children {
+		sj.Children = append(sj.Children, c.toStopwatchJSON())
+	}
+	return
+}
+
+func (s *Stopwatch) UnmarshalJSON(text []byte) error {
+	var j stopwatchJSON
+	if err := json.Unmarshal(text, &j); err != nil {
+		return err
+	}
+	j.toStopwatch(s)
+	return nil
+}
+
+func (s Stopwatch) MarshalJSON() ([]byte, error) {
+	return json.Marshal(s.toStopwatchJSON())
+}
+
+func DurationMinimalistFormat(d time.Duration) string {
+	if d < time.Microsecond {
+		return strconv.Itoa(int(d)) + "ns"
+	} else if d < time.Millisecond {
+		return strconv.Itoa(int(d/1e3)) + "µs"
+	} else if d < time.Second {
+		return strconv.Itoa(int(d/1e6)) + "ms"
+	}
+	return strconv.Itoa(int(d/1e9)) + "s"
 }
--- time_test.go
+++ time_test.go
@@ -1,6 +1,7 @@
 package astikit
 
 import (
+	"bytes"
 	"context"
 	"encoding/json"
 	"errors"
@@ -66,5 +67,114 @@ func TestNow(t *testing.T) {
 	m.Close()
 	if g := Now(); !isAbsoluteTime(Now()) {
 		t.Fatalf("expected %s to be an absolute time", g)
+	}
+}
+
+func TestTimestampNano(t *testing.T) {
+	const j = `{"value":1732636645443709000}`
+	v := struct {
+		Value TimestampNano `json:"value"`
+	}{}
+	err := json.Unmarshal([]byte(j), &v)
+	if err != nil {
+		t.Fatalf("err should be nil, got %s", err)
+	}
+	if e, g := int64(1732636645443709000), v.Value.UnixNano(); g != e {
+		t.Fatalf("timestamp should be %v, got %v", e, g)
+	}
+	b, err := json.Marshal(v)
+	if err != nil {
+		t.Fatalf("err should be nil, got %s", err)
+	}
+	if string(b) != j {
+		t.Fatalf("json should be %s, got %s", j, b)
+	}
+}
+
+func TestStopwatch(t *testing.T) {
+	var count int64
+	defer MockNow(func() time.Time {
+		count++
+		return time.Unix(count, 0)
+	}).Close()
+
+	s1 := NewStopwatch()
+	s2 := s1.NewChild("1")
+	if e, g := 2*time.Second, s1.Duration(); e != g {
+		t.Fatalf("expected %s, got %s", e, g)
+	}
+	s2.Done()
+	s1.NewChild("2")
+	s3 := s1.NewChild("3")
+	s3.NewChild("3-1")
+	s4 := s3.NewChild("3-2")
+	s4.NewChild("3-2-1")
+	s5 := NewStopwatch()
+	s5.NewChild("3-2-2")
+	s6 := s5.NewChild("3-2-3")
+	s6.NewChild("3-2-3-1")
+	s5.Done()
+	s4.Merge(s5)
+	s3.NewChild("3-3")
+	s1.NewChild("4")
+	s1.Done()
+	if e, g := `16s
+  [1s]1: 2s
+  [4s]2: 1s
+  [5s]3: 10s
+    [6s]3-1: 1s
+    [7s]3-2: 7s
+      [8s]3-2-1: 2s
+      [10s]3-2-2: 1s
+      [11s]3-2-3: 2s
+        [12s]3-2-3-1: 1s
+    [14s]3-3: 1s
+  [15s]4: 1s`, s1.Dump(); e != g {
+		t.Fatalf("expected %s, got %s", e, g)
+	}
+	if e, g := 16*time.Second, s1.Duration(); e != g {
+		t.Fatalf("expected %s, got %s", e, g)
+	}
+	b, err := s5.MarshalJSON()
+	if err != nil {
+		t.Fatalf("expected no error, got %s", err)
+	}
+	if e, g := []byte(`{"children":[{"children":[],"created_at":11000000000,"done_at":12000000000,"label":"3-2-2"},{"children":[{"children":[],"created_at":13000000000,"done_at":14000000000,"label":"3-2-3-1"}],"created_at":12000000000,"done_at":14000000000,"label":"3-2-3"}],"created_at":10000000000,"done_at":14000000000,"label":""}`), b; !bytes.Equal(e, g) {
+		t.Fatalf("expected %s, got %s", e, g)
+	}
+	var s7 Stopwatch
+	if err = s7.UnmarshalJSON(b); err != nil {
+		t.Fatalf("expected no error, got %s", err)
+	}
+	if e, g := *s5, s7; !reflect.DeepEqual(e, g) {
+		t.Fatalf("expected %+v, got %+v", e, g)
+	}
+}
+
+func TestDurationMinimalistFormat(t *testing.T) {
+	for _, v := range []struct {
+		d time.Duration
+		e string
+	}{
+		{
+			d: 123 * time.Nanosecond,
+			e: "123ns",
+		},
+		{
+			d: 123456 * time.Nanosecond,
+			e: "123µs",
+		},
+		{
+			d: 123456789 * time.Nanosecond,
+			e: "123ms",
+		},
+		{
+			d: 123456789123 * time.Nanosecond,
+			e: "123s",
+		},
+	} {
+		if g := DurationMinimalistFormat(v.d); v.e != g {
+			t.Fatalf("expected %s, got %s", v.e, g)
+		}
 	}
 }
